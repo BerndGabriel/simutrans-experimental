BerndGabriel/simutrans-experimental

commit 34d5dead5b94d61f6dfc59fa8714dfba141ef0d3

Half size on savegames in networkmode with trees
git-svn-id: svn://simutrans/trunk@4338 8aca7d54-2c30-db11-9de9-000461428c89

boden/tunnelboden.cc
@@ -22,7 +22,7 @@
 
 tunnelboden_t::tunnelboden_t(karte_t *welt, loadsave_t *file, koord pos ) : boden_t(welt, koord3d(pos,0), 0)
 {
-	rdwr(file);
+	grund_t::rdwr(file);	// since it has no trees, we do not need to invoke boden_t
 
 	// some versions had tunnel without tunnel objects
 	if (!find<tunnel_t>()) {
@@ -34,9 +34,7 @@ tunnelboden_t::tunnelboden_t(karte_t *welt, loadsave_t *file, koord pos ) : bode
 }
 
 
-
-void
-tunnelboden_t::calc_bild_internal()
+void tunnelboden_t::calc_bild_internal()
 {
 	// tunnel mouth
 	if (ist_karten_boden()) {
@@ -80,9 +78,7 @@ tunnelboden_t::calc_bild_internal()
 }
 
 
-
-void
-tunnelboden_t::rdwr(loadsave_t *file)
+void tunnelboden_t::rdwr(loadsave_t *file)
 {
 	xml_tag_t t( file, "tunnelboden_t" );
 
@@ -107,6 +103,7 @@ tunnelboden_t::rdwr(loadsave_t *file)
 		}
 	}
 }
+
 
 void tunnelboden_t::info(cbuffer_t & buf) const
 {

besch/baum_besch.h
@@ -57,6 +57,7 @@ public:
 	// old style trees and new style tree support ...
 	int get_seasons() const
 	{
+
 		if(number_of_seasons==0) {
 			return get_child<bildliste2d_besch_t>(2)->get_anzahl() / 5;
 		}

boden/boden.cc
@@ -12,9 +12,12 @@
 #include "../simskin.h"
 #include "../simintr.h"
 
+#include "../dings/baum.h"
+
 #include "../gui/ground_info.h"
 #include "../gui/karte.h"
 #include "../dataobj/umgebung.h"
+#include "../dataobj/loadsave.h"
 
 #include "boden.h"
 #include "wege/strasse.h"
@@ -28,9 +31,54 @@
 #include "../tpl/ptrhashtable_tpl.h"
 
 
-boden_t::boden_t(karte_t *welt, koord3d pos,hang_t::typ sl) : grund_t(welt, pos)
+boden_t::boden_t(karte_t *welt, loadsave_t *file, koord pos ) : grund_t( welt, koord3d(pos,0) )
+{
+	grund_t::rdwr( file );
+
+	// restoring trees (disadvantage: loosing offsets but much smaller savegame footprint)
+	if(  file->get_version()>=110001  ) {
+		sint16 id = file->rd_obj_id();
+		while(  id!=-1  ) {
+			sint32 age;
+			file->rdwr_long( age );
+			// check, if we still have this tree ... (if there are not trees, the first index is NULL!)
+			if(  baum_t::get_anzahl_besch()>id  &&  (*(baum_t::get_all_besch()))[id]!=NULL  ) {
+				baum_t *tree = new baum_t( welt, get_pos(), (uint16)id, age, slope );
+				dinge.add( tree );
+				dbg->warning( "boden_t::boden_t()", "Could not restore tree type %i at (%s)", id, pos.get_str() );
+			}
+			// check for next tree
+			id = file->rd_obj_id();
+		}
+	}
+}
+
+
+boden_t::boden_t(karte_t *welt, koord3d pos, hang_t::typ sl) : grund_t(welt, pos)
 {
 	slope = sl;
+}
+
+
+// only for more compact saving of trees
+void boden_t::rdwr(loadsave_t *file)
+{
+	grund_t::rdwr(file);
+
+	if(  file->get_version()>=110001  ) {
+		if(  umgebung_t::networkmode  &&  !hat_wege()  ) {
+			for(  uint8 i=0;  i<dinge.get_top();  i++  ) {
+				ding_t *d = dinge.bei(i);
+				if(  d->get_typ()==ding_t::baum  ) {
+					baum_t *tree = (baum_t *)d;
+					file->wr_obj_id( tree->get_besch_id() );
+					sint32 age = tree->get_age();
+					file->rdwr_long( age );
+				}
+			}
+		}
+		file->wr_obj_id( -1 );
+	}
 }
 
 

boden/boden.h
@@ -24,8 +24,10 @@ protected:
 	virtual void calc_bild_internal();
 
 public:
-	boden_t(karte_t *welt, loadsave_t *file, koord pos ) : grund_t( welt, koord3d(pos,0) ) { rdwr(file); }
+	boden_t(karte_t *welt, loadsave_t *file, koord pos );
 	boden_t(karte_t *welt, koord3d pos, hang_t::typ slope);
+
+	virtual void rdwr(loadsave_t *file);
 
 	inline bool ist_natur() const { return !hat_wege()  &&  !is_halt(); }
 

dataobj/dingliste.cc
@@ -737,18 +737,23 @@ ding_t *dingliste_t::get_convoi_vehicle() const
 
 void dingliste_t::rdwr(karte_t *welt, loadsave_t *file, koord3d current_pos)
 {
-	sint32 max_object_index;
-	if(file->is_saving()) {
-		max_object_index = top-1;
-	}
-	file->rdwr_long(max_object_index);
+	if(file->is_loading()) {
 
-	if(max_object_index>254) {
-		dbg->error("dingliste_t::laden()","Too many objects (%i) at (%i,%i), some vehicle may not appear immediately.",max_object_index,current_pos.x,current_pos.y);
-	}
+		sint32 max_object_index;
+		if(  file->get_version()<=110000  ) {
+			file->rdwr_long(max_object_index);
+			if(max_object_index>254) {
+				dbg->error("dingliste_t::laden()","Too many objects (%i) at (%i,%i), some vehicle may not appear immediately.",max_object_index,current_pos.x,current_pos.y);
+			}
+		}
+		else {
+			uint8 obj_count;
+			file->rdwr_byte(obj_count);
+			max_object_index = -1+(sint32)obj_count;
+		}
 
-	for(sint32 i=0; i<=max_object_index; i++) {
-		if(file->is_loading()) {
+
+		for(sint32 i=0; i<=max_object_index; i++) {
 			ding_t::typ typ = (ding_t::typ)file->rd_obj_id();
 			// DBG_DEBUG("dingliste_t::laden()", "Thing type %d", typ);
 
@@ -995,8 +1000,14 @@ void dingliste_t::rdwr(karte_t *welt, loadsave_t *file, koord3d current_pos)
 				add(d);
 			}
 		}
-		else {
-			// here is the saving part ...
+	}
+	else {
+		/* here is the saving part ...
+		 * first: construct a lsit of stuff really neded to save
+		 */
+		ding_t *save[256];
+		sint32 max_object_index = 0;
+		for(  uint16 i=0;  i<top;  i++  ) {
 			ding_t *d=bei(i);
 			if(d->is_way()
 				// do not save smoke
@@ -1008,28 +1019,40 @@ void dingliste_t::rdwr(karte_t *welt, loadsave_t *file, koord3d current_pos)
 				// do not save factory buildings => factory will reconstruct them
 				||  (d->get_typ()==ding_t::gebaeude  &&  ((gebaeude_t *)d)->get_fabrik())
 				// things with convoi will not be saved
-				||  (d->get_typ()>=66  &&  d->get_typ()<82)
+				||  (d->get_typ()>=66  &&  d->get_typ()<82
+				||  (umgebung_t::networkmode  &&  d->get_typ()==ding_t::baum  &&  file->get_version()>=110001)  )
 			) {
 				// these objects are simply not saved
-				file->wr_obj_id(-1);
 			}
 			else {
-				// on old versions
-				if(d->get_pos()==current_pos) {
-					file->wr_obj_id(d->get_typ());
-					d->rdwr(file);
-				}
-				else if (d->get_pos().get_2d() == current_pos.get_2d()) {
-					// ok, just error in z direction => we will correct it
-					dbg->warning( "dingliste_t::rdwr()","position error: z pos corrected on %i,%i from %i to %i", d->get_pos().x, d->get_pos().y, d->get_pos().z, current_pos.z);
-					file->wr_obj_id(d->get_typ());
-					d->set_pos(current_pos);
-					d->rdwr(file);
-				}
-				else {
-					dbg->error("dingliste_t::rdwr()","unresolvable position error: %i,%i instead %i,%i (object type %i will be not saved!)", d->get_pos().x, d->get_pos().y, current_pos.x, current_pos.y, d->get_typ());
-					file->wr_obj_id(-1);
-				}
+				save[max_object_index++] = d;
+			}
+		}
+		// now we know the number of stuff to save
+		max_object_index --;
+		if(  file->get_version()<=110000  ) {
+			file->rdwr_long( max_object_index );
+		}
+		else {
+			uint8 obj_count = max_object_index+1;
+			file->rdwr_byte( obj_count );
+		}
+		for(sint32 i=0; i<=max_object_index; i++) {
+			ding_t *d = save[i];
+			if(d->get_pos()==current_pos) {
+				file->wr_obj_id(d->get_typ());
+				d->rdwr(file);
+			}
+			else if (d->get_pos().get_2d() == current_pos.get_2d()) {
+				// ok, just error in z direction => we will correct it
+				dbg->warning( "dingliste_t::rdwr()","position error: z pos corrected on %i,%i from %i to %i", d->get_pos().x, d->get_pos().y, d->get_pos().z, current_pos.z);
+				file->wr_obj_id(d->get_typ());
+				d->set_pos(current_pos);
+				d->rdwr(file);
+			}
+			else {
+				dbg->error("dingliste_t::rdwr()","unresolvable position error: %i,%i instead %i,%i (object type %i will be not saved!)", d->get_pos().x, d->get_pos().y, current_pos.x, current_pos.y, d->get_typ());
+				file->wr_obj_id(-1);
 			}
 		}
 	}

dings/baum.cc
@@ -78,13 +78,13 @@ vector_tpl<weighted_vector_tpl<uint32> > baum_t::baum_typen_per_climate(MAX_CLIM
  */
 stringhashtable_tpl<const baum_besch_t *> baum_t::besch_names;
 
+
 // total number of trees
 // the same for a certain climate
 int baum_t::get_anzahl_besch(climate cl)
 {
 	return baum_typen_per_climate[cl].get_count();
 }
-
 
 
 /**
@@ -127,7 +127,6 @@ uint8 baum_t::plant_tree_on_coordinate(karte_t * welt, koord pos, const uint8 ma
 	}
 	return 0;
 }
-
 
 
 /**
@@ -175,7 +174,6 @@ bool baum_t::plant_tree_on_coordinate(karte_t * welt, koord pos, const baum_besc
 	}
 	return false;
 }
-
 
 
 uint32 baum_t::create_forest(karte_t *welt, koord new_center, koord wh )
@@ -239,7 +237,6 @@ DBG_MESSAGE("verteile_baeume()","distributing single trees");
 }
 
 
-
 static bool compare_baum_besch(const baum_besch_t* a, const baum_besch_t* b)
 {
 	/* Gleiches Level - wir führen eine künstliche, aber eindeutige Sortierung
@@ -258,6 +255,10 @@ bool baum_t::alles_geladen()
 		stringhashtable_iterator_tpl<const baum_besch_t*> iter(besch_names);
 		while(  iter.next()  ) {
 			baum_typen.insert_ordered( iter.get_current_value(), compare_baum_besch );
+			if(  baum_typen.get_count()==254  ) {
+				dbg->error( "baum_t::alles_geladen()", "Maximum tree count exceeded! (max 254 instead of %i)", besch_names.get_count() );
+				break;
+			}
 		}
 		// fill the vector with zeros
 		for (uint8 j=0; j<MAX_CLIMATES; j++) {
@@ -276,7 +277,6 @@ bool baum_t::alles_geladen()
 }
 
 
-
 bool baum_t::register_besch(baum_besch_t *besch)
 {
 	// avoid duplicates with same name
@@ -288,14 +288,13 @@ bool baum_t::register_besch(baum_besch_t *besch)
 }
 
 
-
 // calculates tree position on a tile
 // takes care of slopes
-void baum_t::calc_off()
+void baum_t::calc_off( uint8 slope )
 {
 	int liob;
 	int reob;
-	switch (welt->lookup( get_pos())->get_grund_hang() ) {
+	switch( slope ) {
 		case 0:
 			liob=simrand(30)-15;
 			reob=simrand(30)-15;
@@ -331,7 +330,6 @@ void baum_t::calc_off()
 			break;
 	}
 }
-
 
 
 void baum_t::calc_bild()
@@ -371,7 +369,6 @@ void baum_t::calc_bild()
 }
 
 
-
 image_id baum_t::get_bild() const
 {
 	// alter/2048 is the age of the tree
@@ -384,7 +381,6 @@ image_id baum_t::get_bild() const
 }
 
 
-
 // image which transparent outline is used
 image_id baum_t::get_outline_bild() const
 {
@@ -392,6 +388,11 @@ image_id baum_t::get_outline_bild() const
 	return get_besch()->get_bild_nr( season, baum_alter );
 }
 
+
+const sint32 baum_t::get_age() const
+{
+	return  welt->get_current_month()-geburt;
+}
 
 
 /* also checks for distribution values
@@ -412,7 +413,6 @@ uint16 baum_t::random_tree_for_climate_intern(climate cl)
 }
 
 
-
 baum_t::baum_t(karte_t *welt, loadsave_t *file) : ding_t(welt)
 {
 	season = 0;
@@ -422,34 +422,33 @@ baum_t::baum_t(karte_t *welt, loadsave_t *file) : ding_t(welt)
 }
 
 
-
 baum_t::baum_t(karte_t *welt, koord3d pos) : ding_t(welt, pos)
 {
 	// Hajo: auch aeltere Baeume erzeugen
 	geburt = welt->get_current_month() - simrand(703);
 	baumtype = random_tree_for_climate_intern(welt->get_climate(pos.z));
 	season = 0;
-	calc_off();
+	calc_off( welt->lookup( get_pos())->get_grund_hang() );
 	calc_bild();
 }
 
 
-
-baum_t::baum_t(karte_t *welt, koord3d pos, uint16 type) : ding_t(welt, pos)
+baum_t::baum_t(karte_t *welt, koord3d pos, uint16 type, sint32 age, uint8 slope ) : ding_t(welt, pos)
 {
-	geburt = welt->get_current_month();
+	geburt = welt->get_current_month()-age;
 	baumtype = type;
 	season = 0;
-	calc_off();
+	calc_off( slope );
 	calc_bild();
 }
+
 
 baum_t::baum_t(karte_t *welt, koord3d pos, const baum_besch_t *besch) : ding_t(welt, pos)
 {
 	geburt = welt->get_current_month();
 	baumtype = baum_typen.index_of(besch);
 	season = 0;
-	calc_off();
+	calc_off( welt->lookup( get_pos())->get_grund_hang() );
 	calc_bild();
 }
 
@@ -466,7 +465,6 @@ bool baum_t::saee_baum()
 
 	return plant_tree_on_coordinate(welt, k, baum_typen[baumtype], true, false);
 }
-
 
 
 /* we should be as fast as possible for this, because trees are nearly the most common object on a map */

dings/baum.h
@@ -23,11 +23,11 @@
 class baum_t : public ding_t
 {
 private:
+	// type of tree (was 9 but for more compact saves now only 254 different ree types are allowed)
+	uint32 baumtype:8;
+
 	// month of birth
 	sint32 geburt:20;
-
-	// type of tree
-	uint32 baumtype:9;
 
 	uint32 season:3;
 
@@ -41,17 +41,18 @@ private:
 	/**
 	 * Berechnet offsets für gepflanzte Bäume
 	 */
-	void calc_off();
+	void calc_off( uint8 slope );
 
 	static uint16 random_tree_for_climate_intern(climate cl);
 
 	static uint8 plant_tree_on_coordinate(karte_t *welt, koord pos, const uint8 maximum_count, const uint8 count);
+
 public:
 	// only the load save constructor should be called outside
 	// otherwise I suggest use the plant tree function (see below)
 	baum_t(karte_t *welt, loadsave_t *file);
 	baum_t(karte_t *welt, koord3d pos);
-	baum_t(karte_t *welt, koord3d pos, uint16 type);
+	baum_t(karte_t *welt, koord3d pos, uint16 type, sint32 age, uint8 slope );
 	baum_t(karte_t *welt, koord3d pos, const baum_besch_t *besch);
 
 	void rdwr(loadsave_t *file);
@@ -83,6 +84,8 @@ public:
 	void operator delete(void *p);
 
 	const baum_besch_t* get_besch() const { return baum_typen[baumtype]; }
+	const uint16 get_besch_id() const { return baumtype; }
+	const sint32 get_age() const;
 
 	// static functions to handle trees
 

gui/settings_stats.cc
@@ -21,7 +21,7 @@ INIT_NUM( "diagonal_multiplier", pak_diagonal_multiplier);
 */
 
 
-static const char *version[7]=
+static const char *version[8]=
 {
 	"0.99.17",
 	"0.100.0",
@@ -29,7 +29,8 @@ static const char *version[7]=
 	"0.102.1",
 	"0.102.2",
 	"0.102.5",
-	"0.110.0"
+	"0.110.0",
+	"0.110.1"
 };
 
 

commit 0cb8c87ff132449c0168e52dc03aeb1fe632d44c

(z9999) load old savegames again (broken since r4338)
git-svn-id: svn://simutrans/trunk@4759 8aca7d54-2c30-db11-9de9-000461428c89

boden/tunnelboden.cc
@@ -21,7 +21,7 @@ tunnelboden_t::tunnelboden_t(karte_t *welt, loadsave_t *file, koord pos ) : bode
 {
 	xml_tag_t t( file, "tunnelboden_t" );
 
-	grund_t::rdwr(file);	// since it has no trees, we do not need to invoke boden_t
+	rdwr(file);
 
 	// some versions had tunnel without tunnel objects
 	if (!find<tunnel_t>()) {